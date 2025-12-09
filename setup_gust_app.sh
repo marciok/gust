9f3b0a10ae40013c847a646e72e4a8323fa85d7b
Marcio Klepacz <marcioklepacz@gmail.com>
2025-12-09 11:02:17 -0300

    chore: bump version

--- setup_gust_app.sh
+++ setup_gust_app.sh
@@ -39,7 +39,7 @@ sed -i.bak '/defp deps do/,/end/ c\
       {:plug_cowboy, "~> 2.7", only: :prod},\
       {:hackney, "~> 1.9"},\
       {:file_system, "~> 1.1", only: :dev},\
-      {:gust_web, "0.1.21"}\
+      {:gust_web, "0.1.22"}\
     ]\
   end
 ' mix.exs
