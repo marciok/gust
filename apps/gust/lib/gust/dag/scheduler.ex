1004673305b258d250b724a8addee61e80b3996b
Marcio Klepacz <marcioklepacz@gmail.com>
2026-01-14 12:44:34 -0300

    feat: refactor cron scheduler

--- apps/gust/lib/gust/dag/scheduler.ex
+++
@@ -1,9 +0,0 @@
-defmodule Gust.DAG.Scheduler do
-  @moduledoc false
-
-  @callback schedule(%{integer() => {:ok, Gust.DAG.Definition.t()} | {:error, term()}}) :: :ok
-
-  def schedule(dag_defs), do: impl().schedule(dag_defs)
-
-  defp impl, do: Application.get_env(:gust, :dag_scheduler)
-end
