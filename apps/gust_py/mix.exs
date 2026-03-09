bfeeb2a5d531c439ac2cdfb391958c76679e39f3
Marcio Klepacz <marcioklepacz@gmail.com>
2026-03-09 17:25:22 -0300

    feat: include gust_py on publish workflow

--- apps/gust_py/mix.exs
+++ apps/gust_py/mix.exs
@@ -1,10 +1,12 @@
 defmodule GustPy.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :gust_py,
-      version: "0.1.0",
+      version: @version 
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -20,9 +22,6 @@ defmodule GustPy.MixProject do
   defp deps do
     [
       {:gust, in_umbrella: true}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end
