5e4e030ae4a87c4bdb2a0b54712136aca7c80cc9
Marcio Klepacz <marcioklepacz@gmail.com>
2026-03-10 11:27:03 -0300

    chore: add missing dep to publish package

--- apps/gust_py/mix.exs
+++ apps/gust_py/mix.exs
@@ -32,6 +32,7 @@ defmodule GustPy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       gust_dep()
     ]
   end
