6c0652101a66400f2e2d00b0714e05c5fdf258c7
Marcio Klepacz <marcioklepacz@gmail.com>
2026-04-28 19:45:43 -0300

    fix: include display time format on test

--- apps/gust_web/test/test_helper.exs
+++ apps/gust_web/test/test_helper.exs
@@ -15,6 +15,10 @@ Application.put_env(:gust, :dag_run_trigger, GustWeb.DAGRunTriggerMock)
 Application.put_env(:gust, :dag_terminator, GustWeb.DAGTerminatorMock)
 Application.put_env(:gust_web, :mcp_tools, GustWeb.MCPToolsMock)
 Application.put_env(:gust_web, :mcp_resources, GustWeb.MCPResourcesMock)
+Application.put_env(:gust_web, :display_date_format,
+  long: "%H:%M:%S %Y-%m-%d",
+  short: "%H:%M:%S %m/%d"
+)
 
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Gust.Repo, :manual)
