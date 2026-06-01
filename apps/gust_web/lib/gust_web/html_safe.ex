8157780f47f5c0d9bacf1ed511555009b93d2d02
Marcio Klepacz <marcioklepacz@gmail.com>
2026-06-01 17:20:13 -0300

    fix: ensure data is parsed safely

--- apps/gust_web/lib/gust_web/html_safe.ex
+++ apps/gust_web/lib/gust_web/html_safe.ex
@@ -1,11 +1,11 @@
 defimpl Phoenix.HTML.Safe, for: Gust.Flows.Task do
   def to_iodata(task) do
-    Phoenix.HTML.Safe.to_iodata("Task #{task.name}")
+    Phoenix.HTML.Safe.to_iodata(["Task ", task.name])
   end
 end
 
 defimpl Phoenix.HTML.Safe, for: Gust.Flows.Run do
   def to_iodata(run) do
-    Phoenix.HTML.Safe.to_iodata("Run #{run.id}")
+    Phoenix.HTML.Safe.to_iodata(["Run ", run.id])
   end
 end
