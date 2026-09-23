d3175f5f2b7f8b453aca28418a75df68f2f6b6c1
Marcio Klepacz <marcioklepacz@gmail.com>
2026-09-23 15:24:45 -0300

    fix: mox test warning

--- apps/gust_shell/test/test_helper.exs
+++ apps/gust_shell/test/test_helper.exs
@@ -6,8 +6,6 @@ case :os.type() do
     ExUnit.configure(exclude: [linux_only: true])
 end
 
-ExUnit.start()
-
 # Add support directory to code path for test fixtures and helpers
 Code.compile_file("support/shell_fixtures.ex", __DIR__)
 
@@ -15,8 +13,7 @@ Code.compile_file("support/shell_fixtures.ex", __DIR__)
 Application.ensure_all_started(:erlexec)
 Application.ensure_all_started(:mox)
 
-# Set up Mox for mocking
-Mox.defmock(Gust.DAGLoggerMock, for: Gust.DAG.Logger)
-
 # Configure the gust app to use the mock DAG logger
 Application.put_env(:gust, :dag_logger, Gust.DAGLoggerMock)
+
+ExUnit.start()
